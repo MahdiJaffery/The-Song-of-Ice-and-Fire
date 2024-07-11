0af7713d976db612aeb629af129ebe306a9106a5
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-07-11 11:55:57 +0300

    route Houses

---
+++ Utilities/Houses.mjs
@@ -0,0 +1,11 @@
+const mockHouses = [{ id: 1, House: 'Targaryen', Words: 'Fire and Blood', Seat: 'Dragonstone', Descent: 'Old Valyria' },
+{ id: 2, House: 'Stark', Words: 'Winter is Coming', Seat: 'Winterfell', Descent: 'The First Men' },
+{ id: 3, House: 'Lanister', Words: 'Hear Me Roar', Seat: 'Casterly Rock', Descent: 'The Andals' },
+{ id: 4, House: 'Baratheon', Words: 'Ours is The Fury', Seat: 'Storm\'s End', Descent: 'The Andals' },
+{ id: 5, House: 'Arryn', Words: 'As High as Honor', Seat: 'Eyrie', Descent: 'The Andals' },
+{ id: 6, House: 'Martell', Words: 'Unbowed, Unbent, Ubroken', Seat: 'Sunspear', Descent: 'The Rhoynar' },
+{ id: 7, House: 'Greyjoy', Words: 'We Do Not Sow', Seat: 'Pike', Descent: 'The First Men' },
+{ id: 8, House: 'Tyrell', Words: 'Growing Strong', Seat: 'Highgarden', Descent: 'The Andals' },
+];
+
+export default mockHouses;
