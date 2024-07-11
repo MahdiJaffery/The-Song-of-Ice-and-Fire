4155dd459013ae058b0aaa332c8f77c61282581f
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-07-11 12:23:21 +0300

    routing

--- ASOIF.mjs
+++ ASOIF.mjs
@@ -15,7 +15,10 @@ app.get('/api/houses', query('filter').optional().isLength({ min: 3, max: 32 }).
     query('value').optional().isLength({ min: 3, max: 32 }).withMessage('Must be 3 - 32 Character long'),
     (request, response) => {
         const { query: { filter, value } } = request;   //  http://localhost:${PORT}/api/houses?filter=filter-value&value=value-value
-        response.send(mockHouses);
+
+        if (!value) return response.status(200).send(mockHouses);
+        if (filter && value) return response.status(200).send(mockHouses.filter((house) => house[filter].includes(value)));
+        return response.sendStatus(400);
     })
 
 app.get('/api/houses/:id', resolveHouseByIndex, (request, response) => {
--- Routes/Houses.mjs
+++ Routes/Houses.mjs
@@ -1,2 +1,11 @@
 import { Router } from 'express';
+import mockHouses from '../Utilities/Houses.mjs';
+import resolveHouseByIndex from '../Utilities/Middlewares.mjs';
 const router = Router();
+
+router.get('/api/houses', (request, response) => {
+    const { query: { filter, value } } = request;
+
+    
+    response.send(mockHouses);
+})
