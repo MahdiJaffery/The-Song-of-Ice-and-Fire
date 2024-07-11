16db24b9059fa4efdf93783376ccb63051e74689
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-07-11 14:35:14 +0300

    commit

--- ASOIF.mjs
+++ ASOIF.mjs
@@ -8,6 +8,7 @@ app.use(houseRouter);
 
 
 app.get('/', (request, response) => {
+    response.cookie('A Cookie for a Pookie', 'Cookie', { maxAge: 60000 });
     response.send('A Song of Ice and Fire');
 })
 
