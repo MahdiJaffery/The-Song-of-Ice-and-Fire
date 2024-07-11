4c278aac4dc13af9a101d22c167b0a2a2f88635c
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-07-11 11:56:28 +0300

    route Houses

--- ASOIF.mjs
+++ ASOIF.mjs
@@ -1,19 +1,10 @@
 import express, { response } from 'express';
 import { query, validationResult } from 'express-validator';
+import mockHouses from './Utilities/Houses.mjs';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
-const mockHouses = [{ id: 1, House: 'Targaryen', Words: 'Fire and Blood', Seat: 'Dragonstone', Descent: 'Old Valyria' },
-{ id: 2, House: 'Stark', Words: 'Winter is Coming', Seat: 'Winterfell', Descent: 'The First Men' },
-{ id: 3, House: 'Lanister', Words: 'Hear Me Roar', Seat: 'Casterly Rock', Descent: 'The Andals' },
-{ id: 4, House: 'Baratheon', Words: 'Ours is The Fury', Seat: 'Storm\'s End', Descent: 'The Andals' },
-{ id: 5, House: 'Arryn', Words: 'As High as Honor', Seat: 'Eyrie', Descent: 'The Andals' },
-{ id: 6, House: 'Martell', Words: 'Unbowed, Unbent, Ubroken', Seat: 'Sunspear', Descent: 'The Rhoynar' },
-{ id: 7, House: 'Greyjoy', Words: 'We Do Not Sow', Seat: 'Pike', Descent: 'The First Men' },
-{ id: 8, House: 'Tyrell', Words: 'Growing Strong', Seat: 'Highgarden', Descent: 'The Andals' },
-];
 
 const resolveHouseByIndex = (request, response, next) => {
     const { params: { id } } = request;
