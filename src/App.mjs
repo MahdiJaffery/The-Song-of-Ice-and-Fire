851d727cf3e0656f4699077ac8af8d31f41bf4ed
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-07-12 11:13:33 +0300

    session

--- src/App.mjs
+++ src/App.mjs
@@ -1,11 +1,13 @@
 import express from 'express';
 import Routes from '../Routes/index.mjs'
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(session());
 app.use(Routes);
 
 app.get('/', (request, resposne) => {
